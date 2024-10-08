bf13fb054f4919a0f79b4cccf295e59c6e89e4b9
LokiMATI <r.matigorov@yandex.ru>
2024-10-08 18:46:08 +0300

    feat (Lesson5): My Solution

---
+++ Course/3rd year/Lesson5/MySolution/Geometry/Geometry/Program.cs
@@ -0,0 +1,11 @@
+﻿using Shape;
+
+
+Console.Write("Введите радиус окружности: "); Circle circle = new Circle(Convert.ToDouble(Console.ReadLine()));
+Console.WriteLine("Площадь окружности: " + circle.GetArea() + "\nПериметр окружности: " + circle.GetPerimeter() + "\nДиаметр окружности: " + circle.GetDiameter());
+
+Console.Write("Введите стороны прямоугольника: "); Square square = new Square(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+Console.WriteLine("Площадь прямоугольника: " + square.GetArea() + "\nПериметр прямоугольника: " + square.GetPerimeter());
+
+Console.Write("Введите стороны треугольника: "); Triangle triangle = new Triangle(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+Console.WriteLine("Площадь треугольника: " + triangle.GetArea() + "\nПериметр треугольника: " + triangle.GetPerimeter());
